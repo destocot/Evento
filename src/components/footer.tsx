32db8420551dbb770f2ef30319a67b2ad564b738
destocot <khurramcali@gmail.com>
2024-04-15 23:06:18 -0400

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   return (
     <footer className="mt-auto flex h-16 items-center justify-between border-t border-white/10 px-3 text-xs text-white/25 sm:px-9">
       <small className="text-xs">
-        &copy; {new Date().getFullYear()} ByteGrad. All rights reserved.
+        &copy; {new Date().getFullYear()} Khurram Ali. All rights reserved.
       </small>
       <ul className="flex gap-x-3 sm:gap-x-8">
         {routes.map((route) => (
